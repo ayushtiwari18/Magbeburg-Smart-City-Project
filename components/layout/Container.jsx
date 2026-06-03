b60bd353b2dfa3c49b0a14645e5dd30dc86536c8
Ayush Tiwari <90121562+ayushtiwari18@users.noreply.github.com>
2026-06-03 20:42:26 +0530

    cleanup: remove old .jsx files replaced by .tsx

--- components/layout/Container.jsx
+++
@@ -1,9 +0,0 @@
-export default function Container({ children, className = "" }) {
-  return (
-    <div
-      className={`mx-auto max-w-[1440px] px-6 sm:px-10 lg:px-20 ${className}`}
-    >
-      {children}
-    </div>
-  );
-}
